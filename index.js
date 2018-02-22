84dffcc409d3b2a88487ba0cb5d02e36e6fb0c00
Susumu Yamazaki <zacky1972@gmail.com>
2018-02-23 02:51:23 +0900

    fix a problem of production and watch options

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ module.exports = function GulpParcel(...options)
     }
 
     options.watch = {
-        value: options.watch ? options.watch : false
+        value: (typeof(options.watch) == "undefined") ? false : options.watch
     };
     options.production = {
-        value: (typeof(options.production) == "undefined") ? (!options.watch || true) : options.production
+        value: (typeof(options.production) == "undefined") ? !options.watch : options.production
     };
     const isTmp = options.outDir ? false : true;
     options.outDir = options.outDir ? options.outDir : ('.tmp-gulp-compile-' + pid);
